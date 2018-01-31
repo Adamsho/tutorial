7967a8d90f211adc88bb58cd34b8f412e4a454b7
Adamsho <bobojon.rich@gmail.com>
2018-01-31 11:27:48 -0500

    Changed DryRun

--- BHPhoto1/src/test/java/com/bh/runner/CukesRunner.java
+++ BHPhoto1/src/test/java/com/bh/runner/CukesRunner.java
@@ -9,7 +9,7 @@ import cucumber.api.junit.Cucumber;
 		features = "./src/test/resources/feature",
 		glue = "com/bh/step_definitions",
 		tags = "@Regression",// @BHphoto  @Smoke
-		dryRun = false
+		dryRun = true
 		)//by default is false.
 //test -Dcucumber.options="--tags @Smoke" THIS WILL OVERRIDE THETAG WE HAVE IN OUR
 // CUCUS RUNNER AND USES THE ONE WE PROVIDED.
